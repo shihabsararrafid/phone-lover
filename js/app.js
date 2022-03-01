fb79e416497e1b2350448bbc844461f64d705eb0
shiabsararrafid <shrafid.532@gmail.com>
2022-03-01 19:45:03 +0600

    showResult function added

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
-const showResult = () => {
+// function for getting the input from text and fetching the url according the search result
+const getInputText = () => {
     const searchedTextElement = document.getElementById('searched-text');
     const searchedText = searchedTextElement.value;
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchedText}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => showResult(data.data))
     searchedTextElement.value = "";
+}
+const showResult = (items) => {
+    if (items.length === 0) {
+        console.log('No phone Found')
+    }
+    for (const singItem of items) {
+        console.log(singItem)
+    }
+    console.log(items)
 }
