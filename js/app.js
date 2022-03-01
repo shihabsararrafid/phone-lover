f363062644de124393b5bbe9ab19b240c614b61b
shiabsararrafid <shrafid.532@gmail.com>
2022-03-01 21:15:47 +0600

    added card

--- js/app.js
+++ js/app.js
@@ -9,11 +9,41 @@ const getInputText = () => {
     searchedTextElement.value = "";
 }
 const showResult = (items) => {
+    const divContainer = document.getElementById('searchResult-container');
+    divContainer.textContent = "";
+    // condition if there is no phone
     if (items.length === 0) {
-        console.log('No phone Found')
+
+        const div2 = document.getElementById('noPhone');
+        console.log(div2)
+        div2.innerHTML = `
+        <div class="w-[100%] text-center mx-auto">
+        <h1 class="  text-5xl text-[#00293B]" >NO PHONE FOUND</h1>
+        </div>
+        
+        `
     }
-    for (const singItem of items) {
-        console.log(singItem)
+    // condition if the search result is valid
+    else {
+        const divContainer = document.getElementById('searchResult-container');
+        divContainer.textContent = "";
+        for (const singItem of items) {
+            console.log(singItem)
+
+            const div = document.createElement('div');
+            div.classList.add("searchResult");
+            div.innerHTML = `
+            <img class="block mx-auto"  src="${singItem.image}" alt="" srcset="">
+            <div class ="mt-8">
+            <h1 class="text-3xl text-center font-semibold">Brand: ${singItem.brand}</h1>
+            <p class="text-3xl text-center font-semibold">Model: ${singItem.phone_name}</p>    
+            </div>
+                  
+            `
+
+            divContainer.appendChild(div)
+        }
     }
-    console.log(items)
+
+
 }
